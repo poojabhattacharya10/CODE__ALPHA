cea790ddacbb1abc5269e4af24559fb8e39fa539
Pooja Bhttacharya <125653047+poojabhattacharya10@users.noreply.github.com>
2023-09-10 22:55:05 +0530

    Add files via upload

---
+++ TASK2/BACKGROUND_GENERATOR/script.js
@@ -0,0 +1,36 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+const random = document.querySelector('button');
+
+function setGradient() {
+    body.style.background = 
+    "linear-gradient(to right,"
+    + color1.value
+    + ", "
+    + color2.value
+    + ")";
+
+    css.textContent = body.style.background + ",";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+////Copied code for random button generator
+randomColor = () => {
+	let letters = '0123456789ABCDEF';
+	let color = '#';
+	for (let i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+};
+
+random.addEventListener('click', () => {
+	color1.value = randomColor();
+	color2.value = randomColor();
+	setGradient();
+});
